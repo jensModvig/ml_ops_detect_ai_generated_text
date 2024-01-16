291944ba35e0d5de43acaa020c9d49ee8d722ffb
DanielHolmelund <danielholmelund@hotmail.com>
2024-01-16 13:00:22 +0100

    Setting dvc remote storage

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -15,15 +15,26 @@ def main():
 
     st.markdown("Alternatively, you can upload a text file. Please make sure that the file is in .txt format.")
     tab1, tab2 = st.tabs(["Text input", "File upload"])
+    valid_input = False
     with tab1:
         user_input = st.text_input("Please enter the text you want to check for AI-generated content")
+        if user_input:
+            valid_input = True
 
     with tab2:
         uploaded_file = st.file_uploader("Choose a file")
         # read the uploaded file
         if uploaded_file is not None:
+            # Check if file is in .txt format
+            if uploaded_file.type != "text/plain":
+                st.error("Please upload a text file in .txt format")
+                st.stop()
             uploaded_file.seek(0)
             user_input = uploaded_file.read().decode("utf-8")
+            valid_input = True
+
+    if not valid_input:
+        st.stop()
     
     # Display text for user
     st.markdown("You entered the following text:")
@@ -34,8 +45,6 @@ def main():
 
 
     st.markdown("To test a new input simply enter a new text or upload a new file.")
-
-        
 
 if __name__ == "__main__":
     main() 
